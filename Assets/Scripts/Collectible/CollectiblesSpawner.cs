6dd4fa31383a38b12c5261cf074f2f81e42283be
Javokhir Nazarov <nazarov7mu@gmail.com>
2022-08-11 20:47:41 +0500

    Update CollectiblesSpawner.cs
    Don't spawn Collectible near the player

--- Assets/Scripts/Collectible/CollectiblesSpawner.cs
+++ Assets/Scripts/Collectible/CollectiblesSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using Managers;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -10,23 +11,47 @@ namespace Collectible
     
         [Header("Prefabs")]
         [SerializeField] private Collectible _collectiblePrefab;
+
+        private GameObject _player;
     
         // To spawn only within this range in playing field
         private const float RangeY = 3.5f;
         private const float RangeX = 2.5f;
-    
-        public void SpawnCollectible()
+
+        private const float MinDistance = 1.5f;
+
+        private void Awake()
         {
-            float posX = Random.Range(-RangeX, RangeX);
-            float posY = Random.Range(-RangeY, RangeY);
-        
-            Collectible collectible = Instantiate(_collectiblePrefab, new Vector2(posX, posY), Quaternion.identity);
-            collectible.Init(this, _scoreManager);
+            _player = GameObject.FindGameObjectWithTag("Player");
         }
 
         private void Start()
         {
             SpawnCollectible();
         }
+        
+        public void SpawnCollectible()
+        {
+            float posX, posY, dist;
+            GetRandomPosition();
+            
+            while (dist < MinDistance)
+            {
+                GetRandomPosition();
+                print("Collectible is too close");
+            }
+
+            void GetRandomPosition()
+            {
+                posX = Random.Range(-RangeX, RangeX);
+                posY = Random.Range(-RangeY, RangeY);
+
+                dist = Vector3.Distance(new Vector3(posX, posY, 0), _player.transform.position);
+            }
+            
+            Collectible collectible = Instantiate(_collectiblePrefab, new Vector2(posX, posY), Quaternion.identity);
+            collectible.Init(this, _scoreManager);
+        }
+
     }
 }
